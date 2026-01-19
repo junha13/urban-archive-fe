e60273ff4665c8ea0a667c57edab4e43d997915c
junha13 <junha13@wavus.co.kr>
2026-01-20 00:46:41 +0900

    auth / view 정리 및 LoginView 생성

---
+++ src/views/MainView.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+<h2>dddd</h2>
+</div>
+
+</template>
+<script setup>
+import { ref, onMounted } from 'vue'
+
+
+
+</script>
---
+++ src/views/auth/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    안녕하세요 로그인 하세요
+</div>
+<div>
+    <input v-model="a" placeholder="id">
+</div>
+<div>
+    <input v-model="a" placeholder="ps">
+</div>
+</template>
+<script setup>
+
+
+</script>
