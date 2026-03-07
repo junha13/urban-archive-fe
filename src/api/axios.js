4a824dda8c71c20c965825ddc797bdc1f62b9945
junha13 <tnznf1113@gmail.com>
2026-03-07 22:02:53 +0900

    feat: axios api 토큰 주입, 권환 체크 설정

---
+++ src/api/axios.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL_LOCAL,
+    withCredentials: true,
+    timeout: 5000, 
+})
+
+// 모든 요청에 token 주입
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config;
+})
+
+// 모든 응답을 확인해 권한 체크 => 로그인 창으로 보내기
+api.interceptors.response.use(
+    (res) => {
+        return res;
+    },
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            // 로그인 페이지 보내기
+        }
+        return Promise.reject(error);
+    })
+
+export default api;
